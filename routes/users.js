dad548a7bc70c950407f2b2b9c3eb6166e1ff968
BMHaste <chriswang2121@gmail.com>
2024-08-24 22:00:36 -0400

    Major edits

---
+++ routes/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use( express.static(path.join(__dirname, '..', 'views')));
+router.use( express.static(path.join(__dirname, '..', 'pictures')));
+router.use( express.static('../validation.js'))
+
+router
+    .route("/signup")
+    .get((req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'views', 'signup.html'));
+    })
+    .post(async (req, res) => {
+        const { username, password } = req.body;
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).sendFile(path.join(__dirname, '..', 'views', 'signupFail.html'));
+        }
+        const newUser = new User({ username, password });
+        await newUser.save();
+    
+        res.sendFile(path.join(__dirname, '..', 'views', 'signupSuccess.html'));
+      });
+
+router
+    .route("/login")
+    .get((req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+    })
+    .post(async (req, res) => {
+        
+      });
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/userDataDB');
+const userSchema = new mongoose.Schema({
+      username: String,
+      password: String,
+    });
+const User = mongoose.model('User', userSchema);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const userRouter = require('./routes/users')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname)));
+
+app.use('/users', userRouter)
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
