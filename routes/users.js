8f22584dbdb141884f7af4a80c525c66b348ec84
BMHaste <chriswang2121@gmail.com>
2024-08-27 20:07:53 -0400

    Completed leaderboard and user adding sets and weights of their workout

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,28 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use( express.static(path.join(__dirname, '..', 'views')));
 router.use( express.static(path.join(__dirname, '..', 'pictures')));
 router.use( express.static('../validation.js'))
+router.use(session({
+    secret: 'chungus',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {secure: false}
+}));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/userDataDB');
+const userSchema = new mongoose.Schema({
+      username: String,
+      password: String,
+      workouts: [String],
+      weights: [Number],
+    });
+const User = mongoose.model('User', userSchema);
 
 router
     .route("/signup")
@@ -17,7 +34,7 @@ router
         const { username, password } = req.body;
         const existingUser = await User.findOne({ username });
         if (existingUser) {
-            return res.status(400).sendFile(path.join(__dirname, '..', 'views', 'signupFail.html'));
+            return res.sendFile(path.join(__dirname, '..', 'views', 'signupFail.html'));
         }
         const newUser = new User({ username, password });
         await newUser.save();
@@ -34,18 +51,48 @@ router
         const { username, password } = req.body;
         const existingUser = await User.findOne({ username, password });
         if (existingUser) {
-            return res.status(400).render('homepage', { username: req.body.username })
+            req.session.username = req.body.username;
+            return res.redirect('/users/homepage')
         }else{
             res.sendFile(path.join(__dirname, '..', 'views', 'incorrect.html'));
         }
       });
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/userDataDB');
-const userSchema = new mongoose.Schema({
-      username: String,
-      password: String,
+router
+    .route("/homepage")
+    .get((req, res) => {
+        return res.render('homepage', { username: req.session.username })
+    })
+    .post(async (req, res) => {
+        const {workout, weight } = req.body;
+        current_user = req.session.username;
+        let user = await User.findOne({ username: current_user });
+        if (user) {
+            user.workouts.push(workout);
+            user.weights.push(weight);
+            await user.save();
+            req.session.workouts = user.workouts;
+            req.session.weights = user.weights;
+            return res.redirect('/users/workouts')
+        }else{
+            res.status(500).send('Internal Server Error');
+        }
+      });
+router
+    .route("/workouts")
+    .get((req, res) => {
+        return res.render('workouts', { workouts: req.session.workouts, weights: req.session.weights })
+    })
+    .post(async (req, res) => {
     });
-const User = mongoose.model('User', userSchema);
+router
+    .route("/leaderboard")
+    .get( async (req, res) => {
+        const users = await User.find({})
+        return res.render('leaderboard', {users})
+    })
+    .post(async (req, res) => {
+    });
+
+
 
 module.exports = router;
