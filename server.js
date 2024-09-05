ad830160580bd8b3d1a7abba67b6faf7ae7d8b79
BMHaste <chriswang2121@gmail.com>
2024-09-04 22:02:09 -0400

    small changes

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
   res.redirect('/users/signup');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
